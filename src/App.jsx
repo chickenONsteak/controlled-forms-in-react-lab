74748624f3249043735a613c14c3cb72dd28184f
chickenONsteak <austinchenwd@gmail.com>
2025-07-26 17:33:19 +0800

    styling left

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // src/App.jsx
-// import Bookshelf from "./Bookshelf.jsx";
+import Bookshelf from "./components/Bookshelf/Bookshelf";
 
 const App = () => {
   return (
     <>
       <h1>My Bookshelf</h1>
-      {/* <Bookshelf /> */}
+      <Bookshelf></Bookshelf>
     </>
   );
 };
--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 
 const Bookshelf = () => {
-  const [books, setBooks] = useState("");
-  const [newBook, setNewBook] = useState(""); // newBook will be an object with <title> key and <author> key
+  const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState({ title: "", author: "" }); // newBook will be an object with <title> key and <author> key
 
   const handleInputChange = (event) => {
-    setNewBook({ title: event.target.value, author: event.target.value });
+    // if (event.target.name === "title") {
+    //   setNewBook((prevState) => {
+    //     prevState.title = event.target.value;
+    //   });
+    // } else {
+    //   setNewBook((prevState) => {
+    //     prevState.author = event.target.value;
+    //   });
+    // }
+    setNewBook((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setBooks((prevState) => [...prevState, newBook]);
+    setNewBook({ title: "", author: "" }); // empty newBook state
   };
 
   return (
@@ -22,13 +35,35 @@ const Bookshelf = () => {
             <label>Title:</label>
             <input
               type="text"
+              name="title"
+              id="title"
               onChange={(event) => handleInputChange(event)}
-              value={newBook}
+              value={newBook.title}
             />
           </div>
+          <div>
+            <label>Author: </label>
+            <input
+              type="text"
+              name="author"
+              id="author"
+              onChange={(event) => handleInputChange(event)}
+              value={newBook.author}
+            />
+          </div>
+          <button onClick={(event) => handleSubmit(event)}>Add Book</button>
         </form>
       </div>
-      <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+      <div className="bookCardsDiv">
+        {books.map((obj, idx) => {
+          return (
+            <div className="bookCard" key={idx}>
+              <p>{obj.title}</p>
+              <p>{obj.author}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
